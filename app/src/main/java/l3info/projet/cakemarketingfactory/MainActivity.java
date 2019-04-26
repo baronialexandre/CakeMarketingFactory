d019b1eb33551edc33fcc26172f9950a6f0a5d29
baronialexandre <kebabman@hotmail.fr>
2019-04-26 12:48:03 +0200

    login screen ihm pour test

--- app/src/main/java/l3info/projet/cakemarketingfactory/MainActivity.java
+++ app/src/main/java/l3info/projet/cakemarketingfactory/MainActivity.java
@@ -1,5 +1,6 @@
 package l3info.projet.cakemarketingfactory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void onSubmitButtonClicked(View view) {
+        EditText emailField = findViewById(R.id.login_email);
+        EditText passwordField = findViewById(R.id.login_password);
+        TextView feedbackTextView = findViewById(R.id.login_feedback_message);
+
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+       feedbackTextView.setVisibility(View.VISIBLE);
+
+        // Authenticate
+        //userService.authenticate(email, password, feedbackTextView, this);
+    }
+
+    public void onRegisterButtonClicked(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 
     @Override
