908d78a429c55c247c0d10c37b0a21a195823c2c
Loic Forestier <loic.forestier.dev@gmail.com>
2019-05-02 18:00:12 +0200

    Refactor "MainActivity" en "LogInActivity" "activity_main" en "activity_login"

--- app/src/main/java/l3info/projet/cakemarketingfactory/LogInActivity.java
+++ app/src/main/java/l3info/projet/cakemarketingfactory/LogInActivity.java
@@ -1,22 +1,14 @@
 package l3info.projet.cakemarketingfactory;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     Context context;
 
@@ -27,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         context = this;
         bRegister = findViewById(R.id.bMainRegister);
@@ -40,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Changer d'activity
-                Intent intentApp = new Intent(MainActivity.this, LoadingActivity.class);
-                MainActivity.this.startActivity(intentApp);
+                Intent intentApp = new Intent(LogInActivity.this, LoadingActivity.class);
+                LogInActivity.this.startActivity(intentApp);
             }
         });
 
@@ -49,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Changer d'activity
-                Intent intentApp = new Intent(MainActivity.this, RegisterActivity.class);
-                MainActivity.this.startActivity(intentApp);
+                Intent intentApp = new Intent(LogInActivity.this, RegisterActivity.class);
+                LogInActivity.this.startActivity(intentApp);
             }
         });
 
