4e5074e6aed31188a629c712b94fdad5595519af
Loic Forestier <loic.forestier.dev@gmail.com>
2019-05-02 17:48:29 +0200

    Refactor + translation "Sign up" en "Register" "sign in" en "Connection"

--- app/src/main/java/l3info/projet/cakemarketingfactory/RegisterActivity.java
+++ app/src/main/java/l3info/projet/cakemarketingfactory/RegisterActivity.java
@@ -3,10 +3,10 @@ package l3info.projet.cakemarketingfactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SignupActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_register);
     }
 }
