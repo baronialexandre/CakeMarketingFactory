3ca326087fcb27880c50d3c382dd79f34dfea0cf
kebabman <kebabman@hotmail.fr>
2019-05-16 15:26:17 +0200

    SellStockTask Toast & WeakReferences

--- app/src/main/java/l3info/projet/cakemarketingfactory/task/SellStockTask.java
+++ app/src/main/java/l3info/projet/cakemarketingfactory/task/SellStockTask.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.lang.ref.WeakReference;
 
 
+import l3info.projet.cakemarketingfactory.R;
 import l3info.projet.cakemarketingfactory.model.Factory;
 import l3info.projet.cakemarketingfactory.utils.Contents;
 import okhttp3.OkHttpClient;
@@ -24,9 +26,10 @@ public class SellStockTask extends AsyncTask<String, Void, Integer> {
     private Factory factory;
     private int productId;
     private int userScore;
-    private TextView stock;
-    private TextView score;
-    private TextView allStock;
+    private int scoreEarned;
+    private WeakReference<TextView> stockView;
+    private WeakReference<TextView> score;
+    private WeakReference<TextView> allStock;
 
     public SellStockTask(long userId, Factory factory, int productId, int userScore, Context ctx, TextView stock, TextView score, TextView allStock) {
         this.userId = userId;
@@ -34,9 +37,9 @@ public class SellStockTask extends AsyncTask<String, Void, Integer> {
         this.productId = productId;
         this.userScore = userScore;
         this.ctx = new WeakReference<>(ctx);
-        this.stock=stock;
-        this.score=score;
-        this.allStock=allStock;
+        this.stockView=new WeakReference<>(stock);
+        this.score=new WeakReference<>(score);
+        this.allStock=new WeakReference<>(allStock);
     }
 
     @Override
@@ -53,8 +56,8 @@ public class SellStockTask extends AsyncTask<String, Void, Integer> {
                 rawJson = response.body().string();
             }
             JSONObject jsonObj = new JSONObject(rawJson);
-            int test = jsonObj.getInt("test");
-            return test;
+            if(jsonObj.has("scoreEarned")) scoreEarned = jsonObj.getInt("scoreEarned");
+            return jsonObj.getInt("test");
         } catch (IOException | JSONException e) {
             return 0;
         }
@@ -69,9 +72,11 @@ public class SellStockTask extends AsyncTask<String, Void, Integer> {
             factory.getCurrentStocks().set(productId,0);
             String stockText = 0+"";
             String allStockText = factory.getCurrentStocks().get(0)+factory.getCurrentStocks().get(1)+factory.getCurrentStocks().get(2)+"/"+(factory.getCapacityLevel()+1)*100;
+            TextView stock = stockView.get();
             stock.setText(stockText);
-            allStock.setText(allStockText);
-            GetScoreTask getScore = new GetScoreTask(userId, score, ctx);
+            allStock.get().setText(allStockText);
+            Toast.makeText(ctx, "You've sold "+scoreEarned+"$ worth of"+productId, Toast.LENGTH_SHORT).show();
+            GetScoreTask getScore = new GetScoreTask(userId, score.get(), ctx);
             getScore.execute();
         }
     }
